0c37ba2dd6a5541c9e3cc42cbc7362eb319dad2d
Roham H <100292931+Rh2001@users.noreply.github.com>
2026-03-31 20:17:07 +0300

    Remove unused Environment variable

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,7 +5,6 @@ dotenv.config({ override: true });
 
 const config = {
 	port: process.env.PORT || 3000,
-	nodeEnv: process.env.NODE_ENV || 'development',
 	db: {
 		host: process.env.DB_HOST || 'localhost',
 		port: Number(process.env.DB_PORT) || 5432,
